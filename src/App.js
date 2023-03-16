a93581ae06ce29436bb724fa9f91de32b4e2092a
flomasta <zatevi@mail.ru>
2023-03-16 23:46:51 +0300

    create PostList app

--- src/App.js
+++ src/App.js
@@ -3,26 +3,30 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import PostItem from "./components/PostItem";
 import '../src/styles/app.css'
+import PostList from "./components/PostList";
 
 
 function App() {
-    const [post, setPosts] = useState([
+    const [posts, setPosts] = useState([
         {id: 1, title: 'Javascript', 'body': 'Javascript - programming language.'},
         {id: 2, title: 'Javascript', 'body': 'Javascript - programming language!'},
         {id: 3, title: 'Javascript', 'body': 'Javascript - programming language!!'},
         {id: 4, title: 'Javascript', 'body': 'Javascript - programming language!!!'},
         {id: 5, title: 'Javascript', 'body': 'Javascript - programming language!!!!'},
     ])
+    const [posts2, setPosts2] = useState([
+        {id: 1, title: 'Python', 'body': 'Python - programming language.'},
+        {id: 2, title: 'Python', 'body': 'Python - programming language!'},
+        {id: 3, title: 'Python', 'body': 'Python - programming language!!'},
+        {id: 4, title: 'Python', 'body': 'Python - programming language!!!'},
+        {id: 5, title: 'Python', 'body': 'Python - programming language!!!!'},
+    ])
     return (
         <div className="App">
             <Counter/>
             <ClassCounter/>
-            <h1 style={{textAlign: 'center'}}>Articles</h1>
-            {post.map(post =>
-                <PostItem post={post} key={[post.id]}/>
-            )}
-
-
+            <PostList posts={posts} title="List of articles"/>
+            <PostList posts={posts2} title="List of articles about Python"/>
         </div>
     );
 }
