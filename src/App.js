08528e9225144ebe75029d67bc821f06e1925840
flomasta <zatevi@mail.ru>
2023-03-16 23:35:12 +0300

    create map function for the list of posts

--- src/App.js
+++ src/App.js
@@ -6,12 +6,21 @@ import '../src/styles/app.css'
 
 
 function App() {
-
+    const [post, setPosts] = useState([
+        {id: 1, title: 'Javascript', 'body': 'Javascript - programming language.'},
+        {id: 2, title: 'Javascript', 'body': 'Javascript - programming language!'},
+        {id: 3, title: 'Javascript', 'body': 'Javascript - programming language!!'},
+        {id: 4, title: 'Javascript', 'body': 'Javascript - programming language!!!'},
+        {id: 5, title: 'Javascript', 'body': 'Javascript - programming language!!!!'},
+    ])
     return (
         <div className="App">
             <Counter/>
             <ClassCounter/>
-            <PostItem post={{id: 1, title: 'Javascript', 'body': 'Javascript - programming language.'}}/>
+            <h1 style={{textAlign: 'center'}}>Articles</h1>
+            {post.map(post =>
+                <PostItem post={post} key={[post.id]}/>
+            )}
 
 
         </div>
