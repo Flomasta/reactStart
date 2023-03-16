9b1a2f352fdd038098b5240b3e9fa5e31b78506c
flomasta <zatevi@mail.ru>
2023-03-16 23:22:50 +0300

    create Post component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, {useState} from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import PostItem from "./components/PostItem";
+import '../src/styles/app.css'
 
 
 function App() {
-
-    const [value, setValue] = useState('Текст в инпуте')
-
 
     return (
         <div className="App">
             <Counter/>
             <ClassCounter/>
+            <PostItem post={{id: 1, title: 'Javascript', 'body': 'Javascript - programming language.'}}/>
+
+
         </div>
     );
 }
