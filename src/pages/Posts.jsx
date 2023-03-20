6747552ddafe1a3fe828b1f3508dbe295280b090
flomasta <zatevi@mail.ru>
2023-03-20 18:14:51 +0300

    infinite scroll

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -12,6 +12,8 @@ import PostFilter from "../components/PostFilter";
 import PostList from "../components/PostList";
 import Pagination from "../components/UI/pagination/Pagination";
 import Loader from "../components/UI/loader/Loader";
+import {useObserver} from "../hooks/useObserver";
+import MySelect from "../components/UI/Select/MySelect";
 
 
 function Posts() {
@@ -30,22 +32,27 @@ function Posts() {
     const [totalPages, setTotalPages] = useState(0)
     const [limit, setLimit] = useState(10)
     const [page, setPage] = useState(1)
+    const lastElement = useRef()
+
 
     const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
         const response = await PostService.getAll(limit, page)
-        setPosts(response.data)
+        setPosts([...posts, ...response.data])
         const totalCount = response.headers['x-total-count']
         setTotalPages(getPagesCount(totalCount, limit))
     });
     const changePage = (page) => {
         setPage(page)
-        fetchPosts(limit, page)
+
 
     }
 
+    useObserver(lastElement, page < totalPages, isPostsLoading, () =>
+        setPage(page + 1)
+    )
     useEffect(() => {
         fetchPosts(limit, page)
-    }, [page])
+    }, [page,limit])
 
     return (<div className="App">
 
@@ -59,13 +66,26 @@ function Posts() {
         <ClassCounter/>
 
         <PostFilter filter={filter} setFilter={setFilter}/>
+        <MySelect value={limit}
+                  onChange={value => setLimit(value)}
+                  defaultValue='Elements on page'
+                  options={[
+                      {value: 5, name: '5'},
+                      {value: 10, name: '10'},
+                      {value: 25, name: '25'},
+                      {value: -1, name: 'Все'},
+                  ]}
+
+        />
         {postError &&
             <h1>Seems that there is some error</h1>
         }
-        {isPostsLoading
-            ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader></Loader></div>
-            : <PostList posts={sortedAndSearchedPosts} removePost={removePost} title="List of articles"/>
+        <PostList posts={sortedAndSearchedPosts} removePost={removePost} title="List of articles"/>
+        <div ref={lastElement} style={{height: 20}}></div>
+        {isPostsLoading &&
+            <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader></Loader></div>
         }
+
 
         <Pagination
             page={page}
